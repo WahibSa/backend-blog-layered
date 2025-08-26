9fc71011aea098f7f64d7b9ab89f0e7e415135d3
ado setiawan <adoraziq@gmail.com>
2025-08-26 22:48:33 +0700

    feat: set up Express server with dotenv configuration and JSON parsing

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
 
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
